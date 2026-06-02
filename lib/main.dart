f5d5023e9f0be604a9b073b3574b27169ed58ff6
Your Name <your@email.com>
2026-06-02 16:42:59 +0300

    created this

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
-import 'package:aaaa/basketCounter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp((Notesapp()));
 }
+
 class Notesapp extends StatelessWidget {
-  const Notesapp ({super.key});
+  const Notesapp({super.key});
 
   @override
   Widget build(BuildContext context) {
