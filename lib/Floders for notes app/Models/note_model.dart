2eb4319902bd4ee940176aa3bc9f60347ed1dd02
wissam dawalibi <your@email.com>
2026-06-07 21:37:30 +0300

    add addnotes method to add note cubit

--- lib/Floders for notes app/Models/note_model.dart
+++ lib/Floders for notes app/Models/note_model.dart
@@ -18,3 +18,24 @@ class NoteModel extends HiveObject {
     required this.Color,
   });
 }
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
+  final String title;
+  @HiveField(1)
+  final String subTitle;
+  @HiveField(2)
+  final String date;
+  @HiveField(3)
+  final int Color;
+
+  NoteModel({
+    required this.title,
+    required this.subTitle,
+    required this.date,
+    required this.Color,
+  });
+}
+
--- lib/Floders for notes app/cubits_Add_note_cubit/Add_notes_cubit.dart
+++ lib/Floders for notes app/cubits_Add_note_cubit/Add_notes_cubit.dart
@@ -1,10 +1,22 @@
+import 'package:aaaa/Floders%20for%20notes%20app/Models/note_model.dart';
+import 'package:aaaa/Floders%20for%20notes%20app/Widgets/Constants.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'Add_notes_state.dart';
 
 class AddNotesCubit extends Cubit<AddNotesState> {
   AddNotesCubit() : super(AddnotesInitial());
-
-
+addnote(NoteModel note) async {
+  emit(AddnotesLoding());
+ try {
+   var notesbox = Hive.box<NoteModel>(kNotesbox);
+   emit(AddnotesSuccess());
+   await notesbox.add(note);
+ }  catch (e) {
+   AddnotesFailure(e.toString());
+ }
 
 }
+
+
 
