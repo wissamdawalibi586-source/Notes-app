668f48f10d6280a725553887e4ca477f5d0e86b5
wissam dawalibi <your@email.com>
2026-06-08 22:51:10 +0300

    create fitch notes methods

--- lib/Floders for notes app/views/notes_view.dart
+++ lib/Floders for notes app/views/notes_view.dart
@@ -1,4 +1,6 @@
+import 'package:aaaa/Floders%20for%20notes%20app/cubits_notes_cubit/notes_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../Widgets/Add_note_buttom_sheet.dart';
 import '../Widgets/Notes_view_Body.dart';
@@ -8,21 +10,23 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: NotesviewBody(),
-      floatingActionButton: FloatingActionButton(
-        shape: CircleBorder(),
-        backgroundColor: Colors.cyanAccent,
-        child: Icon(Icons.add, color: Colors.black),
-        onPressed: () {
-          showModalBottomSheet(
-            isScrollControlled: true,
-            context: context,
-            builder: (context) {
-              return AddNotesbuttonsheet();
-            },
-          );
-        },
+    return BlocProvider(create: (context)=> NotesCubit(),
+      child: Scaffold(
+        body: NotesviewBody(),
+        floatingActionButton: FloatingActionButton(
+          shape: CircleBorder(),
+          backgroundColor: Colors.cyanAccent,
+          child: Icon(Icons.add, color: Colors.black),
+          onPressed: () {
+            showModalBottomSheet(
+              isScrollControlled: true,
+              context: context,
+              builder: (context) {
+                return AddNotesbuttonsheet();
+              },
+            );
+          },
+        ),
       ),
     );
   }
